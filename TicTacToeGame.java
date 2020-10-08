46e0659ed839490cb5273825978dc7bbc7cdc2ac
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-08 10:07:26 +0530

    [Surajmal Kumar].Refactoring.putting the object inside the function.

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
 
 public class TicTacToeGame {
-
-	
-	
-	public static void board() {
+  
+	public static void createBoard() {
 		 char[] Board= new char[10];
 	     for(int index=0;index<10;index++){
 	    	Board[index]=' ';
         	}
 	  }
-
-
-	public static void main(String[] args) {
-	    board();
+     public static void main(String[] args) {
+	    createBoard();
 		}
-}
+    }
